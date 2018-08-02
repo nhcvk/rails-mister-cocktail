b77da3d5b2a93138d310eda32945bbedf826eedb
Niels van Klooster <nielsvanklooster@gmail.com>
2018-08-02 16:59:28 +0800

    have the views, controller for cocktails, doses and ingredients still missing

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,4 +1,12 @@
 class Dose < ApplicationRecord
+
   belongs_to :cocktail
   belongs_to :ingredient
+
+  validates :description, presence: true
+  validates :amount, presence: true
+
+  validates_uniqueness_of :cocktail, scope: :ingredient
+
+
 end
