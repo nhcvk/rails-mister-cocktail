2277eb4372f598261c42c7d41471e65592a3ec7e
Niels van Klooster <nielsvanklooster@gmail.com>
2018-08-03 16:31:17 +0800

    add a seed database for ingredients using JSON

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,9 @@
 Ingredient.create(name: "lemon")
 Ingredient.create(name: "ice")
 Ingredient.create(name: "mint leaves")
+
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+data=JSON.parse(open(url).read)
+
+data["drinks"].each {|i| Ingredient.create(name: i["strIngredient1"])}
